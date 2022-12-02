3e4b4c87034f829ed09453ee6ddead8c4194e3d4
Bibek Pandey <bewakepandey@gmail.com>
2022-12-02 16:36:14 +0545

    Update docker compose, fix tasks

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+run=${1:-server}
+wait_cmd="/code/scripts/wait-for-it.sh $POSTGRES_HOSTNAME:$POSTGRES_PORT"
 
+if [[ "$run" == "server" ]]; then
+    $wait_cmd && \
+        python manage.py migrate && \
+        python manage.py runserver 0.0.0.0:8000
+elif [[ "$run" == "celery" ]]; then
+    $wait_cmd && celery -A core_server worker -l info
+elif [[ "$run" == "beat" ]]; then
+    $wait_cmd && celery -A core_server beat -l info
+else
+    echo "Wrong run command. Provide either 'server', 'celery' or 'beat'"
+    exit 1
+fi
