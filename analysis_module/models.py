c1b573e5a6e895f8cfd31b8f77efa9d372afccda
Bibek Pandey <bewakepandey@gmail.com>
2023-04-12 16:28:32 +0545

    Change url field to text field for larger urls

--- analysis_module/models.py
+++ analysis_module/models.py
@@ -18,7 +18,7 @@ class AnalysisModuleRequest(models.Model):
     client_id = models.CharField(max_length=50)
     status = models.IntegerField(choices=RequestStatus.choices, default=RequestStatus.INITIATED)
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    result_s3_link = models.URLField(null=True, blank=True)
+    result_s3_link = models.TextField(null=True, blank=True)
     type = models.CharField(choices=FeaturesType.choices, max_length=20)
     request_params = models.JSONField(null=True, blank=True)  # To capture the original request params
 
